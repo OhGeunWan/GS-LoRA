6eedcc0c26572c27473fecbe9f7c859b493dfe9e
OhGeunwan <ohguenwan1104@gmail.com>
2025-09-25 09:47:42 +0900

    Update test_sub.sh

--- scripts/test_sub.sh
+++ scripts/test_sub.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-pretrain=./results/ViT-P8S8_casia100_cosface_s1-1200-150de-depth6/Backbone_VIT_Epoch_1110_Batch_82100_Time_2023-10-18-18-22_checkpoint.pth
+pretrain=./results/ViT-P8S8_casia100_cosface_s1-1200-150de-depth6/Backbone_VIT_Epoch_1185_Batch_45020_Time_2024-09-26-03-26_checkpoint.pth
 
 python test/test_own.py -w 0 \
  --batch_size 2 --lora_rank 0 \
